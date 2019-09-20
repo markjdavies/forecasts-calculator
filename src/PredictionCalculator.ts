5313fa427cad41b83738a054d0737ae0af4fb13c
markjdavies <46246921+markjdavies@users.noreply.github.com>
2019-09-20 15:00:39 +0100

    Add prediction calculator tests

--- src/PredictionCalculator.ts
+++ src/PredictionCalculator.ts
@@ -4,25 +4,25 @@ import { PlayerPrediction } from "./Entities/PlayerPrediction";
 import { FootballScore } from "./Entities/FootballScore";
 
 export const calculatePredictionPoints = (prediction: PlayerPrediction, result: FootballScore): PredictionPoints => {
-        if (!result) {
-            return 0;
-        }
+    if (!result) {
+        return 0;
+    }
 
-        if (prediction.homeGoals === result.homeGoals && prediction.awayGoals === result.awayGoals) {
-            if (prediction.turtus) {
-                return PredictionPoints.DoubleTurtus;
-            }
-            else {
-                return PredictionPoints.CorrectScore;
-            }
-        }
-        else if (prediction.turtus) {
-            return PredictionPoints.None;
-        }
-        else if (determineOutcome(prediction.homeGoals, prediction.awayGoals) == determineOutcome(result.homeGoals, result.awayGoals)) {
-            return PredictionPoints.Minstrel;
+    if (prediction.homeGoals === result.homeGoals && prediction.awayGoals === result.awayGoals) {
+        if (prediction.turtus) {
+            return PredictionPoints.DoubleTurtus;
         }
         else {
-            return PredictionPoints.None;
+            return PredictionPoints.CorrectScore;
         }
+    }
+    else if (prediction.turtus) {
+        return PredictionPoints.None;
+    }
+    else if (determineOutcome(prediction.homeGoals, prediction.awayGoals) === determineOutcome(result.homeGoals, result.awayGoals)) {
+        return PredictionPoints.Minstrel;
+    }
+    else {
+        return PredictionPoints.None;
+    }
 }
