dc9648437bc7f33078dadb70a2fb8492e10886ba
markjdavies <46246921+markjdavies@users.noreply.github.com>
2019-09-20 16:57:38 +0100

    Consider result when calculating lucky team score

--- src/LuckyTeamCalculator.ts
+++ src/LuckyTeamCalculator.ts
@@ -1,4 +1,12 @@
-export const calculateLuckyTeamPoints = (luckyTeamGoals: number, turtus: boolean) => {
-    const turtusFactor = turtus ? 2 : 1;
-    return luckyTeamGoals * turtusFactor;
+import { PredictionPoints } from './Entities/PredictionPoints';
+
+export const calculateLuckyTeamPoints = (luckyTeamGoals: number, predictionOutcome: PredictionPoints, turtus: boolean) => {
+    return luckyTeamGoals * getTurtusFactor(predictionOutcome, turtus);
+}
+
+const getTurtusFactor = (predictionOutcome: PredictionPoints, turtus: boolean): number => {
+    if (!turtus) {
+        return 1;
+    }
+    return predictionOutcome === PredictionPoints.DoubleTurtus ? 2 : 0;
 }
